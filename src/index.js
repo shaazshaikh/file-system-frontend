45e2c21520916c3b171574f8d117098e6bd3e9b2
Shaaz Shaikh <shaazshaikh153@outlook.com>
2024-03-29 03:20:37 +0530

    commit - 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function MyFirstFunction() {
-  return <h2>My first component - updated</h2>;
+  return <h2>My first component - updated - 2</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
